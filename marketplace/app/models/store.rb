5877cc0bd00822581d115490542700b200314863
JefBNascimento <jbnascimento@msn.com>
2020-06-03 16:11:55 -0300

    Config Atlas Mongodb

---
+++ marketplace/app/models/store.rb
@@ -0,0 +1,8 @@
+class Store
+  include Mongoid::Document
+  field :name, type: String
+  field :website, type: String
+  field :logo, type: String
+  field :email, type: String
+  has_many :products, dependent: :destroy
+end
